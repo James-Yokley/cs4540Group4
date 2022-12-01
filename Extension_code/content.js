4fce0d8ea81701efe2024fe68d48edd0754d8247
James-Yokley <jyokley@calstatela.edu>
2022-12-01 10:58:31 -0800

    Daltonize Filter Implementation
    Implemented the daltonization filters to increase contrast for protanopia and deuteranopia.

--- Extension_code/content.js
+++ Extension_code/content.js
@@ -4,6 +4,8 @@ b.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	if(message.action == "bodyClass") {
 		document.body.classList.remove("protanopia");
 		document.body.classList.remove("deuteranopia");
+		document.body.classList.remove("deuteranopia_daltonize"); 
+		document.body.classList.remove("protanopia_daltonize"); 
 		if(message.class != ""){					
 			console.log(`message.class: ${message.class}`); 
 			document.body.classList.add(message.class);
